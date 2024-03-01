e618e05d42c9928ca4272e55eddab7e56b94627c
suleymankayir <suleymantalhakayir@gmail.com>
2024-03-01 12:05:04 +0300

    rename commit

--- src/main/java/dev/patika/VetManagement/core/exception/NoExistanceException.java
+++
@@ -1,7 +0,0 @@
-package dev.patika.VetManagement.core.exception;
-
-public class NoExistanceException extends IllegalStateException{
-    public NoExistanceException(String s) {
-        super(s);
-    }
-}
---
+++ src/main/java/dev/patika/VetManagement/core/exception/NoExistenceException.java
@@ -0,0 +1,7 @@
+package dev.patika.VetManagement.core.exception;
+
+public class NoExistenceException extends IllegalStateException{
+    public NoExistenceException(String s) {
+        super(s);
+    }
+}
